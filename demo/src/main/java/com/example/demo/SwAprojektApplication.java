95ae500863830a9caa0a60bfa43724fbe327cf74
Dhami <Koco.Dhami@Student.HTW-Berlin.de>
2023-12-01 18:40:29 +0100

    first commit

---
+++ demo/src/main/java/com/example/demo/SwAprojektApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwAprojektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwAprojektApplication.class, args);
+	}
+
+}
